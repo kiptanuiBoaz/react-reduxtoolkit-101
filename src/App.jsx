ebe161223339aadf0679d43b3e6e4a34a91f12ce
Boaz <boaserem022@gmail.com>
2022-08-24 18:18:54 +0300

    redux 02

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-//this is the entry poing
-function App() {
+import "../src/App.css";
+import {useSelector,useDispatch} from "react-redux";
+import { decrement, increment } from "./redux/counter";
+
+
+export default function App() {
+  const {count} = useSelector((state)=> state.counter);
+  const dispatch = useDispatch();
   return (
     <div >
-      <h1>Template</h1>
+      <h1>The count is : {count}</h1>
+      <button onClick={()=> dispatch(increment)}>Increment</button>
+      <button onClick={()=> dispatch(decrement)}>Decrement</button>
     </div>
   );
 }
 
-export default App;
+ 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { store } from "./redux/store";
+import { Provider } from "react-redux"
 
 // this is the entry point of the app
-ReactDOM.render( <App />, document.getElementById("root"));
+ReactDOM.render(
+    <Provider
+        store={store}>
+           <App />
+    </Provider>
+  
+,document.getElementById("root"));
 
 
