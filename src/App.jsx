1ee615a76ebdabedb5216746d092eeb0a125f6af
Boaz <boaserem022@gmail.com>
2022-08-24 18:40:37 +0300

    environ

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { decrement, increment, incrementByAmount } from "./redux/counter";
 export default function App() {
   const {count} = useSelector((state)=> state.counter);
   const dispatch = useDispatch();
-  return (
+  return (  
     <div >
       <h1>The count is : {count}</h1>
       <button onClick={()=> dispatch(increment())}>Increment</button>
